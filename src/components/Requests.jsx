5cb1172d35b990f2b3b4f6aa20f554bc7f02cd38
amiyasingh <23ucc513@lnmiit.ac.in>
2026-06-20 15:13:25 +0530

    F7 - Requests page

--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import { useDispatch, useSelector } from "react-redux";
+import { BASE_URL } from "../utils/constants";
+import axios from "axios";
+import { useEffect } from "react";
+import { addRequests } from "../utils/requestSlice";
 
 const Requests = () => {
+  const requests = useSelector((store) => store.requests);
+  const dispatch = useDispatch();
+  const fetchRequests = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/user/requests/received", {
+        withCredentials: true,
+      });
+      dispatch(addRequests(res.data.data));
+    } catch (err) {}
+  };
+  useEffect(() => {
+    fetchRequests();
+  }, []);
+  if (!requests) return;
+  if (requests.length == 0) return <h1>No Requests Found</h1>;
   return (
-    <div>Requests</div>
-  )
-}
+    <div className="text-center my-10">
+      <h1 className="text-bold text-3xl">Requests</h1>
 
-export default Requests
+      {requests.map((request) => {
+        const { _id, firstName, lastName, photoUrl, age, gender, about } =
+          request.fromUserId;
+        return (
+          <div
+            key={_id}
+            className="flex justify-between items-center m-4 p-4 rounded-lg bg-base-300 w-1/2 mx-auto"
+          >
+            <div>
+              <img
+                alt="photo"
+                className="w-20 h-20 rounded-full"
+                src={photoUrl}
+              />
+            </div>
+            <div className="text-left mx-4">
+              <h2 className="font-bold text-xl">
+                {firstName + " " + lastName}
+              </h2>
+              <p>{about}</p>
+            </div>
+            <div>
+              <button className="btn btn-primary mx-2">👍</button>
+              <button className="btn btn-secondary mx-2">👎🏻</button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+  return <div>Requests</div>;
+};
+
+export default Requests;
